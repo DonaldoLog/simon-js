bab1aad5efc568f7a48f53f94d991f681a95580d
Donaldo Lopez <donaldolog@gmail.com>
2019-02-24 13:46:41 -0600

    bjetos

--- clase5.js
+++ clase5.js
@@ -1,8 +1,26 @@
-var nombre = 'Donaldo'
-
-function imprimirNombreEnMayus() {
-    nombre = nombre.toUpperCase()
-    console.log(nombre)
+var donaldo = {
+  nombre: 'Donaldo',
+  apellido: 'Lopez',
+  edad: 24
 }
 
-imprimirNombreEnMayus()
+var dario = {
+  nombre: 'Dario',
+  apellido: 'Frndz',
+  edad: 28
+}
+
+function imprimirNombreEnMayus(persona) {
+  var { nombre, edad } = persona
+  console.log(`Hola me llamo ${nombre} y tengo ${edad}`)
+}
+
+imprimirNombreEnMayus(donaldo)
+imprimirNombreEnMayus(dario)
+
+function cumpleanos(persona) {
+  return {
+    ...persona,
+    edad: persona.edad + 1
+  }
+}
