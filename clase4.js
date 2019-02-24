b2169682e0fd954e25b6744bf9c33459191bd654
Donaldo Lopez <donaldolog@gmail.com>
2019-02-24 00:13:22 -0600

    funcion

---
+++ clase4.js
@@ -0,0 +1,11 @@
+var nombre = 'Donaldo', edad = 24
+
+function imprimirEdad(n, e) {
+  console.log(`${n} tiene ${e} años`);
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Juan', 28)
+imprimirEdad('Tesla', 21)
+imprimirEdad(12, 'Carlos')
+imprimirEdad('Juan')
